8832c7f7ed73dc34aab3f135a7997a50cee40474
Ishanh2000 <misraishanh2000@gmail.com>
2020-01-08 23:02:54 +0530

    Restarting

---
+++ tasks.v
@@ -0,0 +1,12 @@
+// AUM ShREE GANESHAAYA NAMAH||
+// Experiment_1 - Design basic NAND, NOR, NOT (CMOS directly compatible) (>=2 inputs)
+// Experimetn_2 - Design AND, OR, XOR, XNOR ann Full_adder  (>=2 inputs)
+// Experiment_3 - Other combinational logic you may think of
+// Experoment_4 - Sequential design: Clock (varying duty cycle), JKFF, DFF, TFF with reset button
+// Experiment_5 - Full addition using no/busses, ripple_carry_adder (no complex algo), then search for a complex algo to add
+// Experiment_6 - Subtraction made possible using addition itself
+// Experiment_7 - Multiplexer, encoder, decoder (rigorous analysis).
+// Experiment_8 - Multiplier / Divider.
+
+
+// Do learn and test syntaxes by yourself.
